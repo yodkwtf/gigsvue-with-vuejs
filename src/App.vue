87261b08923fa3a143991ec2561d273a66bf92ce
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-11-09 04:07:30 +0530

    added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
-<script setup></script>
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+</script>
 
 <template>
-  <h1 class="text-center text-2xl text-blue-500">JobVue</h1>
+  <Navbar />
 </template>
