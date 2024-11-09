f400018a3218bbbdcf08c80616107ccd232b96e4
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-11-09 22:57:45 +0530

    created hero section with props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
 </script>
 
 <template>
   <Navbar />
+  <!-- TODO: Change props for branding -->
+  <Hero title="Test Title" subtitle="Test Subtitle" />
 </template>
