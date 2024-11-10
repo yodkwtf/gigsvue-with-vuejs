0557c11fdbe34e4a7599aeebfa7fe3955fabcd18
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2024-11-10 16:41:35 +0530

    job listings component implemented with mock data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
-import HomeCard from '@/components/HomeCard.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
   <!-- TODO: Change props for branding -->
   <Hero title="Test Title" subtitle="Test Subtitle" />
-  <HomeCard />
+  <HomeCards />
+  <JobListings />
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,22 @@
+<script setup>
+import JobListing from './JobListing.vue';
+import jobData from '@/config/jobs.json';
+import { ref } from 'vue';
+
+const jobs = ref(jobData);
+console.log(jobs.value);
+</script>
+
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-blue-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+      </div>
+    </div>
+  </section>
+</template>
