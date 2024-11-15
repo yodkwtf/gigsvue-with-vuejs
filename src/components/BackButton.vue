291a577c54c4b6c107232e955dcfc0486a377b39
yodkwtf <48durgesh.kumar@gmail.com>
2024-11-15 23:10:33 +0530

    created custom go back button for job view

---
+++ src/components/BackButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <section>
+    <div class="container m-auto py-4 px-6">
+      <RouterLink
+        to="/jobs"
+        class="text-teal-500 hover:text-teal-600 flex items-center"
+      >
+        <i class="pi pi-arrow-circle-left mr-3"></i>Back to Jobs
+      </RouterLink>
+    </div>
+  </section>
+</template>
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -3,6 +3,7 @@ import { reactive, onMounted } from 'vue';
 import { useRoute, RouterLink } from 'vue-router';
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
+import BackButton from '@/components/BackButton.vue';
 
 const route = useRoute();
 const jobId = route.params.id; // the param name in jobs.json is 'id'
@@ -25,6 +26,8 @@ onMounted(async () => {
 </script>
 
 <template>
+  <BackButton />
+
   <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
     <PulseLoader color="teal" />
   </div>
