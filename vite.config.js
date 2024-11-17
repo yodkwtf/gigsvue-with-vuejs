7595836172d48b70f521413267e49dc19ec8c587
yodkwtf <48durgesh.kumar@gmail.com>
2024-11-17 22:19:11 +0530

    fixed API URL for prod

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // API server
+        target:
+          'https://my-json-server.typicode.com/yodkwtf/gigsvue-with-vuejs', // API server
+        // target: 'http://localhost:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
